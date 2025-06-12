ed1f1fa100b50b1e0b6d013a2b86ffba0e31ae72
MendosDV <mendos.prod@gmail.com>
2025-06-12 16:38:43 -0400

    Add style

---
+++ first_app/app/_layout.jsx
@@ -0,0 +1,30 @@
+import { Slot, Stack } from 'expo-router'
+import { StyleSheet, View, Text, useColorScheme } from "react-native"
+import { Colors } from '../constants/Colors'
+import { StatusBar } from 'expo-status-bar'
+
+const RootLayout = () => {
+    const colorScheme = useColorScheme()
+    console.log(colorScheme)
+    const theme = Colors[colorScheme] ?? Colors.light
+    
+    return (
+        <>
+            <StatusBar value='auto' />
+            <Stack screenOptions={{
+                headerStyle: { backgroundColor: theme.navBackground },
+                headerTintColor: theme.title,
+            }}>
+                <Stack.Screen name='index' options={{ title: 'Home' }} />
+                <Stack.Screen name='about' options={{ title: 'About' }} />
+                <Stack.Screen name='contact' options={{ title: 'Contact', headerShown: false }} />
+            </Stack>
+        </>
+    )
+}
+
+export default RootLayout
+
+const styles = StyleSheet.create({
+
+})
--- first_app/app/about.jsx
+++ first_app/app/about.jsx
@@ -1,11 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, useColorScheme } from "react-native"
 import { Link } from 'expo-router'
+import { Colors } from '../constants/Colors'
 
 const About = () => {
+    const colorScheme = useColorScheme()
+    const theme = Colors[colorScheme] ?? Colors.light
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>About Page</Text>
-            <Link href={'/'} style={styles.link}>Back home</Link>
+        <View 
+            style={[styles.container, { backgroundColor: theme.background }]}
+
+        >
+            <Text style={[styles.title, { color: theme.text }]}>About Page</Text>
+            <Link 
+                href={'/'} 
+                style={[
+                    styles.link, 
+                    { 
+                        color: theme.title,
+                        borderColor: theme.title 
+                    }
+                ]}
+            >
+                Back home
+            </Link>
         </View>    
     )
 }
