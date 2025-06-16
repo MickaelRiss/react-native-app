7dd02c888d5cd19d5991ab3c9f2e9a56328c3ab0
MendosDV <mendos.prod@gmail.com>
2025-06-15 20:44:17 -0400

    display error message

--- first_app/app/(auth)/login.jsx
+++ first_app/app/(auth)/login.jsx
@@ -7,20 +7,23 @@ import ThemedButton from '../../components/ThemedButton'
 import ThemedTextInput from '../../components/ThemedTextInput'
 import { useState } from "react";
 import { useUser } from '../../hooks/useUser'
-
+import { Colors } from '../../constants/Colors'
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const { user, login } = useUser()
+    const [ error, setError ] = useState(null)
 
     const handleSubmit = async () => {
+        setError(null)
+
         try {
             console.log('Login form submitted')
             await login(email, password)
             console.log('Current user is: ', user)
         } catch(error) {
-            console.log(error.message)
+            setError(error.message)
         }
     }
 
@@ -57,6 +60,9 @@ const Login = () => {
                     <Text style={{ color: '#f2f2f2', textAlign: 'center' }}>Login</Text>
                 </ThemedButton>
 
+                <Spacer height={50}/>
+                {error && <Text style={styles.error}>{error}</Text>}
+
                 <Spacer height={100}/>
                 
                 <Link href='/register'>
@@ -82,4 +88,13 @@ const styles = StyleSheet.create({
         fontSize: 18,
         marginBottom: 30
     },
+    error: {
+        color: Colors.warning,
+        padding: 20,
+        backgroundColor: '#f5c1c8',
+        borderColor: Colors.warning,
+        borderWidth: 1,
+        borderRadius: 6,
+        marginHorizontal: 10,
+    }
 })
--- first_app/app/(auth)/register.jsx
+++ first_app/app/(auth)/register.jsx
@@ -7,19 +7,23 @@ import ThemedButton from '../../components/ThemedButton'
 import ThemedTextInput from '../../components/ThemedTextInput'
 import { useState } from "react";
 import { useUser } from "../../hooks/useUser";
+import { Colors } from '../../constants/Colors'
 
 const Register = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const { user, register } = useUser()
+    const [error, setError] = useState(null)
 
     const handleSubmit = async() => {
+        setError(null)
+
         try {
             console.log('Register form submitted')
             await register(email, password)
             console.log('Current user is: ', user)
         } catch(error) {
-            console.log(error.message)
+            setError(error.message)
         }
 
     }
@@ -57,6 +61,9 @@ const Register = () => {
                 <ThemedButton onPress={handleSubmit} width="25%">
                     <Text style={{ color: '#f2f2f2', textAlign: 'center' }}>Register</Text>
                 </ThemedButton>
+
+                <Spacer height={50}/>
+                {error && <Text style={styles.error}>{error}</Text>}
                 
                 <Spacer height={100}/>
                 
@@ -83,4 +90,13 @@ const styles = StyleSheet.create({
         fontSize: 18,
         marginBottom: 30
     },
+    error: {
+        color: Colors.warning,
+        padding: 20,
+        backgroundColor: '#f5c1c8',
+        borderColor: Colors.warning,
+        borderWidth: 1,
+        borderRadius: 6,
+        marginHorizontal: 10,
+    }
 })
